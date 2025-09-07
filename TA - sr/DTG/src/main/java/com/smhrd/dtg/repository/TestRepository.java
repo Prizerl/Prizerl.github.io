230cc109f79aa19fd565b71369a309cf25ad6b08
한승룡 <152689419+Prizerl@users.noreply.github.com>
2025-09-07 23:43:12 +0900

    Delete TA - sr directory

--- TA - sr/DTG/src/main/java/com/smhrd/dtg/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smhrd.dtg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.smhrd.dtg.entity.User;
-
-public interface TestRepository extends JpaRepository<User, Long> {
-}
